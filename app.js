c08e82c72ec20b83680f346c6ad1d7b6f670f1ad
amitkumarsingh12345 <163759129+amitkumarsingh12345@users.noreply.github.com>
2024-07-22 02:55:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,20 @@ app.use(cors());
 
 // ----------------DATABASE CONNECTION---------------------
 
-mongo.connect('mongodb://localhost:27017/testdb').
-    then(() => console.log("DB Connected!!")).
-    catch((err) => console.log({ "Error": err }));
+const conn = async (req, res) => {
+  try {
+    await mongoose
+      .connect(
+        "mongodb://localhost:27017"
+      )
+      .then(() => {
+        console.log("Connected");
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+conn();
 
 app.use(bodyParser.json());
 
