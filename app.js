0cf090fa6a853a30d78e44785bc3f5779b96fdbf
amitkumarsingh12345 <163759129+amitkumarsingh12345@users.noreply.github.com>
2024-07-22 22:14:17 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 const createdb = () => {
    try {
-      mongoose.connect('mongodb+srv://Amitkumarsingh:amit1234@testdb.gk0sdgg.mongodb.net/?retryWrites=true&w=majority&appName=testdb');
+      mongoose.connect('mongodb+srv://Amitkumarsingh:amit1234@testdb.gk0sdgg.mongodb.net/?retryWrites=true&w=majority&appName=testdb/test');
       console.log("Connectiuon Success!!");
    } catch(error) {
       console.log(`Connection Error: ${error}`);
